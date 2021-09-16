24e5e25ee76186c88ebd2598c976b8b54cdab59c
grainchter <grainchter@gmail.com>
2021-09-16 18:19:23 +0700

    Homework-5

--- src/components/routes/Games/Games.js
+++ src/components/routes/Games/Games.js
@@ -1,18 +1,64 @@
+import { useHistory } from 'react-router';
 import style from './Games.module.css';
+import PokemonCard from '../../PokemonCard/PokemonCard';
+import POKEMONS from './../../../json/pokemons.json';
+import { useState } from 'react';
 
-const GamePage = ({ onChangePage }) => {
+const POKEMONDATA = POKEMONS.map(pokemon => {
+    Object.assign(pokemon, { 'active': false })
+    return pokemon;
+})
 
-    const onGamesClick = () => {
-        onChangePage('app');
+const GamePage = () => {
+
+    const history = useHistory();
+
+    const onClick = () => {
+        history.push('/');
     }
 
+    const [pokemons, setPokemons] = useState(() => POKEMONS.slice(0, 5));
+
+    const onClickPokemon = (id) => {
+        setPokemons(prevState =>
+            prevState.map(
+                item => item.id === id ?
+                    { ...item, active: !item.active } :
+                    item
+            )
+        )
+    }
+
+
     return (
-        <div className = {style.center}>
-            <h1>зис из гейм пэйдж</h1>
-            <button onClick={onGamesClick}>
+        <div className={style.root}>
+            <div className={style.center}>
+                <h1>зис из гейм пэйдж</h1>
+
+
+                {
+                    pokemons.map((item) =>
+                        <div className={style.card}>
+                            <PokemonCard
+                                key={item.id}
+                                name={item.name}
+                                img={item.img}
+                                id={item.id}
+                                type={item.type}
+                                values={item.values}
+                                isActive={item.active}
+                                onClickPokemon={onClickPokemon}
+                            />
+                        </div>
+                    )
+                }
+            </div>
+            <button onClick={onClick} >
                 Go to Home Page
             </button>
         </div>
+
+
 
 
 
---
+++ src/components/routes/NotFound/NotFound.js
@@ -0,0 +1,20 @@
+import style from './NotFound.module.css';
+
+const NotFoundPage = () => {
+
+
+
+    return (
+        <div className={style.center}>
+            <h1>404</h1>
+            <button >
+                Go to Home Page
+            </button>
+        </div>
+
+
+
+    );
+}
+
+export default NotFoundPage;
